cc9278d68199cb8ffb9b0b95221b7e0109813d24
Eric Lee <eazylee92@gmail.com>
2023-06-24 16:25:00 -0800

    added a hash map that will return the character that appears the most often by counting whether or not the char in charMap is pressent

--- js/main.js
+++ js/main.js
@@ -18632,3 +18632,24 @@ function jewelsInStones(jewels, stones){
   }
   return count
 }
+
+//given a string of characters, return the character that appears the most often
+
+function appearsMost(str){
+  let charMap = {},
+  count = 0,
+  maxChar = null
+
+  for(const char of str){
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  for(const char in charMap){
+    if(charMap[char] > count){
+      count = charMap[char]
+      maxChar = char
+    }
+  }
+  return maxChar
+}
+
