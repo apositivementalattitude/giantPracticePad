1469bbce3a9f5b1457d00ed5ce6381bd0df164f8
Eric Lee <eazylee92@gmail.com>
2023-07-13 15:42:51 -0800

    added promises to js

--- js/main.js
+++ js/main.js
@@ -18420,3 +18420,14 @@ function comboPoints(a,b,c){
 
 //e.g., :checked
 //pseudo class checked
+
+//promise syntax
+//fetch("#")
+//.then(res => res.json())
+//.then(data => {
+  //console.log(data)
+//})
+//.catch(error => {
+  //console.log(`error${error}`)
+//})
+
