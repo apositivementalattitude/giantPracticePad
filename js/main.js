0b69020962c11822931114c44e7d1d770ee6fe48
Eric Lee <eazylee92@gmail.com>
2023-07-19 00:53:02 -0700

    added string length sorting function, and added some methods to the objects of the string sort function

--- js/main.js
+++ js/main.js
@@ -18722,3 +18722,23 @@ function fizzBuzz(n){
     }
   }
 }
+
+//in the .sort method, what are the items sorted as?
+//strings\
+
+//methods
+//methods are actions that we can perform on objects
+
+//-=
+//decreae value 
+
+//given 2 strings a and b, return a string of the form short+long+short
+//with the shorter string on the outside and the longer string on the inside
+
+function solution (a, b){
+  if(a.length > b.length){
+    return b + a + b
+  }else{
+    return a + b + a
+  }
+}
