07a2686666143703e46e498fb197e4d41102c3d1
Eric Lee <eazylee92@gmail.com>
2023-12-08 10:10:39 -0800

    updated

--- js/main.js
+++ js/main.js
@@ -23110,3 +23110,19 @@ function wordSearch(str, loc, num){
     return arr.join('-')
 }
 
+//ternary operator syntax
+// return thing ? 'true' : 'false'
+
+//count the number of divisors of a positive integer n
+
+function getDivisorsCnt(n){
+    let count = 0
+
+    for(let i = 0; i < n; i++){
+        if(n % i === 0){
+            count++
+        }
+    }
+    return count
+}
+
