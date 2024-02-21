6575a8de11140728e7ccb39e6a2a51eda8cf52fe
Eric Lee <eazylee92@gmail.com>
2024-02-20 18:12:11 -0800

    updated

--- js/main.js
+++ js/main.js
@@ -24689,3 +24689,19 @@ function getGrade(s1, s2, s3){
 //if you are ggoing to write front end code
 //where does it belong?
 //in the view
+
+//create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+    constructor(color, make, model, price){
+        this.color = color
+        this.make = make
+        this.model = model
+        this.price = price
+    }
+    turnOn(){
+        console.log('turned on')
+    }
+}
+
+let coolEspressoMachine = new EspressoMachine('black', "Make", "Pro", "$15,000")
