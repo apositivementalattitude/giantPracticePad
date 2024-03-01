90c88a427b8ec488bafe1f517588457a570aacec
Eric Lee <eazylee92@gmail.com>
2024-02-29 18:59:56 -0800

    updated

--- js/main.js
+++ js/main.js
@@ -24796,11 +24796,21 @@ function howMuchWater(water, load, clothes){
 function sumMul(n, m){
     let sum = 0
 
-    for(let i = 0; i < m; i+=n){
+    for(let i = n; i < m; i+=n){
         if(i % n === 0){
-            sum+=n[i]
+            sum+=i
         }
     }
     return sum
 }
+
+//create a function that has a loop that prints '21' 21 times to the console and then call that function
+
+function twentyOne(){
+    for(let i = 0; i < 21; i++){
+        document.querySelector("#21").innerText += '21'
+    };
+};
+twentyOne();
+//can you make it print '21' 21 times to the DOM?
 
